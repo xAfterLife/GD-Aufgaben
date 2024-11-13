0e7a0cd19ee7582a850a495780d2d4b872657cec
slxshx <leon.linss@outlook.de>
2024-11-13 09:30:25 +0100

    first commit test

---
+++ 1-Konsolen-RPG/Leon/ConsoleRPG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
